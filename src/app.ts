25908d34f4a773b732b0abaef1b99a246b094c54
Ishtiak Al Humaidi <ishtiakalhumaidi@gmail.com>
2026-02-24 19:26:43 +0600

    better auth added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { type Application } from "express";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send("SkillBridge web is cooking...");
